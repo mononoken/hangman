8e3de182da408cc821f276124c33d16d7d9ddf84
mononoken <ken.maeshima@pm.me>
2022-04-28 22:05:29 -0600

    Change request_player_guess such that message displays for incorrect input

--- lib/game.rb
+++ lib/game.rb
@@ -7,7 +7,6 @@ class Game
 
   INCORRECT_LIMIT = 6
 
-  # Game
   def initialize
     @player = Player.new
     reset_previous_choice
@@ -16,8 +15,10 @@ class Game
   end
 
   def request_player_guess
-    puts 'Player input letter:'
-    @player.guess = gets.chomp.downcase until valid_letter?(@player.guess)
+    until valid_letter?(@player.guess)
+      puts 'Player input letter:'
+      @player.guess = gets.chomp.downcase
+    end
   end
 
   def check_guess
@@ -26,6 +27,12 @@ class Game
     else
       @player.log_incorrect
     end
+  end
+
+  def player_guess
+    request_player_guess
+    @previous_choice.push(@player.guess)
+    check_guess
   end
 
   def play_round
@@ -39,12 +46,6 @@ class Game
     @secret_word = SecretWord.new
     play_round until end_game?
     announce_results
-  end
-
-  def player_guess
-    request_player_guess
-    @previous_choice.push(@player.guess)
-    check_guess
   end
 
   def reset_previous_choice
