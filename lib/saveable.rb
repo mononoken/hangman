5f38eff8b0c5e3fa5dffba282b4e665b3eb4bd20
mononoken <ken.maeshima@pm.me>
2022-05-20 19:48:56 -0700

    Complete SaveName and add it to #save_game

--- lib/saveable.rb
+++ lib/saveable.rb
@@ -1,21 +1,26 @@
 # frozen_string_literal: true
 
 # Add saving and loading methods to Game.
+module SaveConstants
+  SAVE_DIR = 'saves'
+  SAVE_FILE = 'save.yaml'
+end
+
 module Saveable
-  SAVE_DIR = 'saves'.freeze
-  SAVE_FILE = 'save.yaml'.freeze
-  SAVE_PATH = "#{SAVE_DIR}/#{SAVE_FILE}".freeze
+  include SaveConstants
 
   def save_game
     Dir.mkdir(SAVE_DIR) unless Dir.exist?(SAVE_DIR)
 
-    File.open(SAVE_PATH, 'w') do |file|
+    File.open(SaveName.new.save_path, 'w') do |file|
       file.puts YAML.dump(self)
     end
     abort 'Game saved'
   end
 
   def load_game
+    SaveName.previous_saves
+    puts 
     YAML.load(File.open(SAVE_PATH, 'r'))
   end
 
@@ -25,23 +30,52 @@ module Saveable
   end
 end
 
+# Represent save file name with proper path formatting.
 class SaveName
-  def save_path
-    saves = self.saves
-    save_name = gets.chomp.downcase until saves.none?(save_name) && ('a'..'z').to_a.all? { |letter| save_name.include? letter }
-    save_file = "#{save_name}.yaml"
-    save_path = "#{SAVE_DIR}/#{save_file}"
-  end
+  include SaveConstants
 
-  def self.saves
-    Dir.glob("#{SAVE_DIR}/*.yaml")
+  def initialize(name = valid_name)
+    @name = name
+  endg
+
+  def save_path
+    "#{SAVE_DIR}/#{save_file}"
   end
 
   def self.acceptable_chars
     ('a'..'z').to_a + ('0'..'9').to_a + ['_']
   end
 
-  def check_name(name)
-    saves.none?(save_name) && name.split('').all? { |letter| acceptable_chars.include? letter }
+  def self.previous_saves
+    Dir.glob("#{SAVE_DIR}/*.yaml")
+  end
+
+  private
+
+  def request_name
+    puts "Please input save file name. Use only letters, numbers, or '_'."
+    gets.chomp.downcase
+  end
+
+  def valid_name
+    name = request_name
+    name = request_name until valid_name?(name)
+    name
+  end
+
+  def save_file
+    "#{@name}.yaml"
+  end
+
+  def valid_name?(name)
+    previous_name?(name) && valid_chars?(name)
+  end
+
+  def previous_name?(name)
+    self.class.previous_saves.none?(name)
+  end
+
+  def valid_chars?(name)
+    name.split('').all? { |letter| self.class.acceptable_chars.include? letter }
   end
 end
